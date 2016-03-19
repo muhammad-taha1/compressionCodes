c35e1690d5400ecf5963cdc61bb095f3c9e404b5
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2016-03-19 16:38:06 -0400

    Fixed issues with generate Codebook
    - Moved the new rows after splitting at the end of CB.
    - Fix written and tested with Bajscy yesterday.
    - Added a complicated example to huffman.m for testing purposes

--- Huffman/generateCodebook.m
+++ Huffman/generateCodebook.m
@@ -13,12 +13,15 @@ for i = length(CB_to_split_indices) : -1 : 1
     % Current index to split
     index_to_split = CB_to_split_indices(i);
     
+    % temp variable to update codebook after split
+    l_of_splitted_cw = l(index_to_split); 
+    
     % split and make new l. Duplicate l at index to split and 
     % add one to increase l for the indices being spit.
     l = [l(1 : index_to_split - 1);
+        l(index_to_split + 1 : end);
         l(index_to_split) + 1;
-        l(index_to_split) + 1;
-        l(index_to_split + 1 : end)];
+        l(index_to_split) + 1];
     
     % Create a column of zeros. To be appended in CB
     zero_column = zeros(length(CB), 1);
@@ -29,15 +32,18 @@ for i = length(CB_to_split_indices) : -1 : 1
     % Create a copy of the row to split from CB
     splitted_row_copy = CB(index_to_split, :);
     
-    % Insert splitted_row_copy in CB at index to split
-    CB = [CB(1 : index_to_split, :);
-        splitted_row_copy;
-        CB(index_to_split + 1 : end, :)];
+    % Change the digit of splitted_row_copy to 1, the bit to 
+    % change specified by L vector. This is the new row to insert to CB.
+    splitted_row_copy(l_of_splitted_cw + 1) = 1;
     
-    % Set the values of row to split as 0 and 1, the vector l defining
-    % the position of where the value has to be changed.
-    CB(index_to_split, l(index_to_split)) = 0;
-    CB(index_to_split + 1, l(index_to_split)) = 1; 
+    % Insert splitted_row_copy in CB. CB now contains all rows from start
+    % to index_to_split - 1, and then the rows after index_to_split. 
+    % The row at index_to_split is then inserted (making it the 2nd last
+    % row), followed by splitted_row_copy.
+    CB = [CB(1 : index_to_split - 1, :);
+        CB(index_to_split + 1 : end, :)
+        CB(index_to_split, :);
+        splitted_row_copy];
     
 end
 
--- Huffman/huffman.m
+++ Huffman/huffman.m
@@ -1,5 +1,6 @@
 function [l, CB] = Huffman(p)
-
+p=[0.3 0.23 0.22 0.2 0.05];
+%p = [ 0.4 .3 .3]
 [l, CB] = combineProbabilities(p, [], false);
 
 lFinal = l
