23361b73eeaf02ec08b48c5ba55dcd54a535ea84
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2016-10-19 17:18:45 -0400

    bug fixed

--- Run_Length/RunLengthTest.m
+++ Run_Length/RunLengthTest.m
@@ -1,13 +1,13 @@
 function RunLengthTest
 % run-length encoding
-max_run_length = 3;
-inputData = rand(1,10)>0.51
+max_run_length = 7;
+probOfZeroes = 0.95;
+inputData = rand(1,10)>probOfZeroes
 encodedRes = Run_Length_Encoder(inputData, max_run_length)%('000000101010101011010111001')
 % calculate prob for Huffman
-[p, keySet] = computeProb(encodedRes, 0.95, max_run_length);
+[p, keySet] = computeProb(encodedRes, probOfZeroes, max_run_length);
 
-p = sort(p,'descend')
-
+p = sort(p,'descend');
 % Huffman encoding
 [HuffmanEncoded, valueSet, l] = encoder(encodedRes, p, keySet)
 % Huffman decoding
--- Run_Length/computeProb.m
+++ Run_Length/computeProb.m
@@ -3,34 +3,39 @@ function [probVector, keySet] = computeProb(runLengthEncodedStr, probOfZero, max
 % and calculates probabilities for each symbol, which will then be used by
 % HuffmanCompression. It also generates KeySet for Huffman
 probVector = [];
-keySet = runLengthEncodedStr(1);
+keySet = [];
 
 for num = 1 : length(runLengthEncodedStr)
-    if (runLengthEncodedStr(num) == max_run_length)
-        % no ones here
-        prob = probOfZero^numOfZeros;
-        probVector = [probVector, prob];
-    else 
-        % get number of zeros for each char
-        numOfZeros = str2num(runLengthEncodedStr(num));
-        % Here we assume that the probability to get 0 is 0.95
-        prob = probOfZero^numOfZeros*(1 - probOfZero);
-        probVector = [probVector, prob];
-    end
     
     % check if current number is in keySet
-    numToAdd = false;
+    numToAdd = true;
     for k = 1 : length(keySet)
-        if (keySet(k) ~= runLengthEncodedStr(num))
-            numToAdd = true; 
+        if (keySet(k) ~= runLengthEncodedStr(num) || length(keySet) == 0)
+            numToAdd = true;
         else
+            % number exists, so break out of the loop
             numToAdd = false;
+            break;
         end
     end
     
     if (numToAdd)
+        % Only add in keyset and compute prob if the number is not in
+        % keyset
         keySet = [keySet, runLengthEncodedStr(num)];
+        
+        if (str2num(runLengthEncodedStr(num)) == max_run_length)
+            % no ones here
+            numOfZeros = str2num(runLengthEncodedStr(num));
+            prob = probOfZero^numOfZeros;
+            probVector = [probVector, prob];
+        else
+            % get number of zeros for each char
+            numOfZeros = str2num(runLengthEncodedStr(num));
+            % Here we assume that the probability to get 0 is 0.95
+            prob = probOfZero^numOfZeros*(1 - probOfZero);
+            probVector = [probVector, prob];
+        end
     end
-    
 end
 end
