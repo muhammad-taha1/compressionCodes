c902ef085c7e6800fa4856e765f29b3bdcce3c55
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2016-10-22 22:55:57 -0400

    sorted keySet according to the sorted values of probability

--- Run_Length/RunLengthTest.m
+++ Run_Length/RunLengthTest.m
@@ -2,18 +2,26 @@ function RunLengthTest
 % run-length encoding
 % Note: max_run_length should be less than or equal to the size of input
 % data
-max_run_length = 24;
-probOfZeroes = 0.85;
-inputData = rand(1,60)>probOfZeroes
+max_run_length = 20;
+probOfZeroes = 0.95;
+inputData = rand(1,32)>probOfZeroes
 encodedRes = Run_Length_Encoder(inputData, max_run_length)%('000000101010101011010111001')
+
 % calculate prob for Huffman
 [p, keySet] = computeProb(encodedRes, probOfZeroes, max_run_length);
 
-p = sort(p,'descend')
+[p, idx] = sort(p,'descend');
+sortedKeySet = [];
+% sort keySet vector according to sorted probabilities index
+for i = 1 : length(keySet)
+    % add keySet value at idx(i) to sortedKeySet
+    sortedKeySet = [sortedKeySet, keySet(idx(i))];
+end
+
 % Huffman encoding
-[HuffmanEncoded, valueSet, l] = encoder(encodedRes, p, keySet)
+[HuffmanEncoded, valueSet, l] = encoder(encodedRes, p, sortedKeySet)
 % Huffman decoding
-HuffmanDecoded = decoder(HuffmanEncoded, keySet, valueSet, l)
+HuffmanDecoded = decoder(HuffmanEncoded, sortedKeySet, valueSet, l)
 % Run-length decoding
 decodedRes = Run_Length_Decoder(HuffmanDecoded, max_run_length)
 
